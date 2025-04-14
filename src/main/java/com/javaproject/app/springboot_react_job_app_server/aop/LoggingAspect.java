e606d2c464d409cb6a61d9d982e90ac8af5292d7
Vedant Karale <vedantkarale271@gmail.com>
2025-04-14 13:39:59 +0530

    JoinPoint

--- src/main/java/com/javaproject/app/springboot_react_job_app_server/aop/LoggingAspect.java
+++ src/main/java/com/javaproject/app/springboot_react_job_app_server/aop/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.javaproject.app.springboot_react_job_app_server.aop;
 
 import lombok.extern.java.Log;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -13,8 +14,8 @@ public class LoggingAspect {
     public static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
     // return type class-name.method-name(args)
-    @Before("execution(* com.javaproject.app.springboot_react_job_app_server.service.JobService.*(..))")
-    public void logMethodCall(){
-        LOGGER.info("Method Called");
+    @Before("execution(* com.javaproject.app.springboot_react_job_app_server.service.JobService.getAllJobPosts(..)) || execution(* com.javaproject.app.springboot_react_job_app_server.service.JobService.getJobPost(..))")
+    public void logMethodCall(JoinPoint jp){
+        LOGGER.info("Method Called "+jp.getSignature().getName());
     }
 }
