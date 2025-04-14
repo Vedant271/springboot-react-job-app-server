7c4ac2c80552accd43cf4318306cae3a2f89dbf7
Vedant Karale <vedantkarale271@gmail.com>
2025-04-14 13:25:48 +0530

    AOP, Before Advice

---
+++ src/main/java/com/javaproject/app/springboot_react_job_app_server/aop/LoggingAspect.java
@@ -0,0 +1,20 @@
+package com.javaproject.app.springboot_react_job_app_server.aop;
+
+import lombok.extern.java.Log;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    public static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+    // return type class-name.method-name(args)
+    @Before("execution(* com.javaproject.app.springboot_react_job_app_server.service.JobService.*(..))")
+    public void logMethodCall(){
+        LOGGER.info("Method Called");
+    }
+}
--- src/main/java/com/javaproject/app/springboot_react_job_app_server/model/JobPost.java
+++ src/main/java/com/javaproject/app/springboot_react_job_app_server/model/JobPost.java
@@ -2,9 +2,7 @@ package com.javaproject.app.springboot_react_job_app_server.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
